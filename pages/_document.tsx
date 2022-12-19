f0578ddb215c45e6bca1f7d413eab7fc5f8eeb6d
sonianedea21 <sonia.nedea21@gmail.com>
2022-12-19 15:18:42 +0200

    feat: scaffolding first page

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import {Head, Html, Main, NextScript} from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head/>
+            <link href='https://fonts.googleapis.com/css?family=Mukta Mahee' rel='stylesheet'/>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
+}
